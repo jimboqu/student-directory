0bb021fba1ce1418b4ca4d8f5db01abc4c92546a
jimboqu <jamesm.quilter@gmail.com>
2018-12-10 09:07:11 +0000

    Refactored directory.rb

--- directory.rb
+++ directory.rb
@@ -1,16 +1,21 @@
 
 student_count = 11
-puts "The student of the academy"
-puts "-----------------"
-puts "Bob Smith"
-puts "John Brown"
-puts "Tracy Beaker"
-puts "Hutch"
-puts "Starskey"
-puts "Donald Duck"
-puts "Billy Bonds"
-puts "Trevor Brooking"
-puts "Pat Holland"
-puts "John Lyall"
-puts "Alvin Martin"
+students = [
+  "Bob Smith",
+  "John Brown",
+  "Tracy Beaker",
+  "Hutch",
+  "Starskey",
+  "Donald Duck",
+  "Billy Bonds",
+  "Trevor Brooking",
+  "Pat Holland",
+  "John Lyall",
+  "Alvin Martin"
+]
+puts "The Academy"
+puts "----------------"
+students.each do |student|
+  puts student
+end
 puts "Overall we have #{student_count} great students"
